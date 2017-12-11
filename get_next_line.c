c10825abbd95179b32ffc4443f370cb2dc01db24
Pablo FAUST <pfaust@e1r6p1.42.fr>
2017-12-11 14:20:20 +0100

    multiple fd fonctionne

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: pfaust <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/05 14:23:24 by pfaust            #+#    #+#             */
-/*   Updated: 2017/12/11 11:45:41 by pfaust           ###   ########.fr       */
+/*   Updated: 2017/12/11 14:19:54 by pfaust           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,32 +33,24 @@ int		get_next_line(const int fd, char **line)
 	int					ret;
 	char				*tmp;
 
-	dprintf(1,"\n\n---------DEBUT DE NOUVEAU GET NEXT LINE ------------\n\n");
 	if (!list)
 		list = ft_newstock(fd);
-	else 
-		dprintf(1,"str static : %s\n",list->str);
 	elem = list;
-	while (elem->fd != fd && elem)
+	while (elem->fd != fd && elem->next)
 		elem = elem->next;
 	if (!elem->next && elem->fd != fd)
 	{
 		elem->next = ft_newstock(fd);
-		//list->next = elem;
 		elem = elem->next;
 	}
 	if ((eol = ft_strchr(elem->str, '\n')))
 	{
-		dprintf(1, "il y a un maillon avec saut a la ligne\n");
 		*line = ft_strsub(elem->str, 0, (eol - elem->str));
-		dprintf(1, "line : %s\n", *line);
 		elem->str = ft_strsub(elem->str, (eol - elem->str), ft_strlen(eol));
-		dprintf(1, "nouveau maillon : %s\n\n", elem->str);
 		return (1);
 	}
 	else
 	{
-		dprintf(1, "il n'y a pas de saut a la ligne dans le maillon\n");
 		while ((ret = read(fd, buf, BUFF_SIZE)))
 		{
 			buf[ret] = '\0';
@@ -66,17 +58,11 @@ int		get_next_line(const int fd, char **line)
 			{
 				tmp = ft_strsub(buf, 0, (eol - buf));
 				*line = ft_strjoin(elem->str, tmp);
-				dprintf(1, "line = %s + %s = %s\n", elem->str, tmp, *line);
-				dprintf(1, "buf = %s\n", buf);
 				elem->str = ft_strsub(buf, (eol - buf + 1), ft_strlen(eol) -1);
-				dprintf(1, "MAJ du maillon : %s\n\n", elem->str);
 				return (1);
 			}
 			else
-			{
 				elem->str = ft_strjoin(elem->str, buf);
-				dprintf(1, "pas de EOL trouve, on stock le buf dans list : %s\n", elem->str);
-			}
 		}
 	}
 	return (0);
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: pfaust <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/05 14:31:26 by pfaust            #+#    #+#             */
-/*   Updated: 2017/12/11 11:13:06 by pfaust           ###   ########.fr       */
+/*   Updated: 2017/12/11 14:17:42 by pfaust           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,24 @@
 #include <fcntl.h>
 int		main(int ac, char **av)
 {
-	int		 fd;
-	char	*line;
+	int		 	fd;
+	int			fd2;
+	char		*line;
 
 	line = NULL;
 	if (ac < 2)
 		return (0);
 	fd = open(av[1], O_RDONLY);
+	fd2 = open(av[2], O_RDONLY);
 	get_next_line(fd, &line);
+	dprintf(1, "%s\n", line);
+	get_next_line(fd2, &line);
 	dprintf(1, "%s\n", line);
 	get_next_line(fd, &line);
 	dprintf(1, "%s\n", line);
-	get_next_line(fd, &line);
-	dprintf(1, "%s\n", line);
-	get_next_line(fd, &line);
-	dprintf(1, "%s\n", line);
-	get_next_line(fd, &line);
-	dprintf(1, "%s\n", line);
-	get_next_line(fd, &line);
+	get_next_line(fd2, &line);
 	dprintf(1, "%s\n", line);
 	close(fd);
+	close(fd2);
 	return (0);
 }
